1fd62f7f6cba4c35398ccce80f2ba059c5d67795
reddy0512 <67320362+TanushReddy2007@users.noreply.github.com>
2021-05-03 08:35:44 +0530

    Add files via upload

---
+++ Project70-main/screens/ReadStoryScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Read extends Component {
+  render() {
+    return (
+      <View>
+        <Text style = {{alignSelf:'center', fontSize:100}}>Read</Text>
+      </View>
+    );
+  }
+}
